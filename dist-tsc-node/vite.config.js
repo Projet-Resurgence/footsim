5f4aecdb0f114f4a7dbacf58f14af027829a3a31
BJ_Bellum <benjamin_claude_@hotmail.com>
2026-06-20 22:32:05 +0200

    fix

--- dist-tsc-node/vite.config.js
+++ dist-tsc-node/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 export default defineConfig({
     plugins: [react()],
-    base: '/footsim/',
+    base: '/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
